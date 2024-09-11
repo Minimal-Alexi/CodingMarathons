f71b7fb5c3c2f9c7802a29fd0dccd8f90566347a
Minimal-Alexi <alexpop@metropolia.fi>
2024-09-11 13:37:51 +0300

    Doing initial setup of app.js

--- CodingMarathon1/src/App.js
+++ CodingMarathon1/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import BookCollectionManager from ''
+import ContactListManager from ''
+import RecipeManager from ''
+import RecipeShoppingCartManager from ''
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{/*       <BookCollectionManager />
+      <ContactListManager />
+      <RecipeManager />
+      <RecipeShoppingCartManager /> */}
     </div>
   );
 }
